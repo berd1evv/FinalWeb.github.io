977ee89b799e584cc282536f8ad96b5a37649506
last_coder <73636880+EB-coder@users.noreply.github.com>
2022-01-06 16:59:48 +0600

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -14,7 +14,12 @@ let weather = {
           }
           return response.json();
         })
-        .then((data) => this.displayWeather(data));
+        .then((data) => this.displayWeather(data) 
+        & this.displayWeather1(data) 
+        & this.displayWeather2(data) 
+        & this.displayWeather3(data) 
+        & this.displayWeather4(data) 
+        & this.displayWeather5(data));
     },
     displayWeather: function (data) {
       const { name, country } = data.city;
@@ -22,6 +27,7 @@ let weather = {
       const { temp, humidity } = data.list[0].main;
       const { speed } = data.list[0].wind;
       const {lon, lat} = data.city.coord;
+      const {dt_txt} = data.list[0];
 
 
       document.querySelector(".city").innerText = "Weather in " + name + ", " + country;
@@ -32,6 +38,77 @@ let weather = {
       document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
       document.querySelector(".lon").innerText = "Longtitude: " + lon;
       document.querySelector(".lat").innerText = "Latitude: " + lat;
+      
+    },
+
+    displayWeather1: function (data) {
+      const { temp} = data.list[7].main;
+      const {dt_txt} = data.list[7];
+      const { icon} = data.list[7].weather[0];
+
+      document.querySelector(".first").innerText = temp + "°C";
+      document.querySelector(".icon1").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".date1").innerText = dt_txt.slice(0, -8);
+
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var now = new Date(dt_txt);
+      document.querySelector(".bir").innerText = days[now.getDay()];
+    },
+
+    displayWeather2: function (data) {
+      const { temp} = data.list[15].main;
+      const {dt_txt} = data.list[15];
+      const { icon} = data.list[15].weather[0];
+
+      document.querySelector(".second").innerText = temp + "°C";
+      document.querySelector(".icon2").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".date2").innerText = dt_txt.slice(0, -8);
+
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var now = new Date(dt_txt);
+      document.querySelector(".eki").innerText = days[now.getDay()];
+    },
+
+    displayWeather3: function (data) {
+      const { temp} = data.list[23].main;
+      const {dt_txt} = data.list[23];
+      const { icon} = data.list[23].weather[0];
+
+      document.querySelector(".third").innerText = temp + "°C";
+      document.querySelector(".icon3").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".date3").innerText = dt_txt.slice(0, -8);
+
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var now = new Date(dt_txt);
+      document.querySelector(".uch").innerText = days[now.getDay()];
+    },
+
+    displayWeather4: function (data) {
+      const { temp} = data.list[31].main;
+      const {dt_txt} = data.list[31];
+      const { icon} = data.list[31].weather[0];
+
+      document.querySelector(".fourth").innerText = temp + "°C";
+      document.querySelector(".icon4").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".date4").innerText = dt_txt.slice(0, -8);
+
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var now = new Date(dt_txt);
+      document.querySelector(".tort").innerText = days[now.getDay()];
+    },
+
+    displayWeather5: function (data) {
+      const { temp} = data.list[39].main;
+      const {dt_txt} = data.list[39];
+      const { icon} = data.list[39].weather[0];
+
+      document.querySelector(".fifth").innerText = temp + "°C";
+      document.querySelector(".icon5").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".date5").innerText = dt_txt.slice(0, -8);
+
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var now = new Date(dt_txt);
+      document.querySelector(".besh").innerText = days[now.getDay()];
     },
 
     search: function () {
